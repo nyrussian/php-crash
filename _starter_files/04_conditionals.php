de31bdfd88a0a1051515eb265b219ceab7ddb11c
nyrussian <nyrussian@hotmail.com>
2024-04-03 18:28:37 -0500

    completed lesson 4

--- _starter_files/04_conditionals.php
+++ _starter_files/04_conditionals.php
@@ -23,7 +23,35 @@ if (condition) {
   // code to be executed if condition is true
 }
 */
+$age = 17;
+// if ($age >=18) {
+//   echo 'you are old enough';
+// }else{
+//   echo 'sorry you are to young';
+// }
 
+$t = date("H")+20;
+// echo $t;
+// IF ($t <12){
+//   echo 'good morning';
+// }elseif($t < 17){
+//   echo 'good afternoon';
+// }  else{
+//   echo 'good eavning';
+// }
+
+// if(false){
+//   echo 234;
+// }else{
+//   echo 556;
+// }
+
+$posts = ['post one', 'post two', 'post three'];
+// if (!empty($posts)){
+//   echo 'no posts found';
+// }else{
+//   echo $posts[0];
+// }
 /* -------- Ternary Operator -------- */
 /*
   The ternary operator is a shorthand if statement.
@@ -31,5 +59,25 @@ if (condition) {
     condition ? true : false;
 */
 
+// $firstpost =  !empty($posts) ? $posts[0] : 'no posts found';
+$firstpost = $posts[1] ?? null;
+// echo $firstpost;
 
 /* -------- Switch Statements ------- */
+
+$favcolor = 'purple';
+// switch ($favcolor) {
+//   case 'red':
+//     echo 'your favorite color is red';
+//     break;
+
+//   case 'blue':
+//       echo 'your favorite color is blue';
+//       break;
+//    case 'green':
+//         echo 'your favorite color is green';
+//         break;
+//     default:
+//     echo 'your favorite color is something else';
+        
+// }
